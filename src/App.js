46ce2ee58180f2ad4e57e8b7d0dbf6ddd9576632
prethiv <prethiv191@gmail.com>
2022-05-29 17:56:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  let [username, setUsername] = useState('');
+  let [password, setPassword] = useState('');
+  let [loginStatus,setLoginStatus] = useState('');
+
+   return (
+    <div>
+      <label>Usrename</label>
+      <input type="text" onChange={(event) => {
+        setUsername(event.target.value)
+      }} />
+      <br />
+      <label>password</label>
+      <input type="text" onChange={(event) => {
+        setPassword(event.target.value)
+      }} />
+      <button
+        onClick={() => {
+          axios.post('http://localhost:3000/register', {
+            username: username,
+            pw: password
+          }).then(data => {
+            console.log(data, 'inside register')
+          })
+            .catch(err => {
+              console.log(err)
+            })
+        }}
+      >Register</button>
+      <button
+          onClick={() => {
+            axios.post('http://localhost:3000/login', {
+              username: username,
+              pw: password
+            }).then(data => {
+              console.log(data, 'inside register')
+              if(data.data==='Username and password matched'){
+               setLoginStatus('Logged in successfully')
+              }
+              else{
+                setLoginStatus('Wrong Password')
+              }
+            })
+              .catch(err => {
+                console.log(err)
+              })
+          }}
+      >Login</button>
+<br/>
+      <label>LoginSTatus</label>
+      {loginStatus}
     </div>
   );
 }
